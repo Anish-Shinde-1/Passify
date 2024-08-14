ac7417076cb98fb29e66fec67d12b088cb87af24
Anish-Shinde-1 <shindeanish2004@gmail.com>
2024-08-14 22:43:21 +0530

    Initial commit with project setup and configuration

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.passify.passify {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires eu.hansolo.tilesfx;
+
+    opens com.passify to javafx.fxml;
+    exports com.passify;
+}
